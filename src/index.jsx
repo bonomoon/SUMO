11e31947b6e2bb0cb2d7455a1d4b8acff01c570d
bonomoon <moonbonoz@gmail.com>
2020-10-11 02:26:11 +0900

    add: apply meterial-ui theme template and add MapPage

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.9.0";
+
+// pages for this product
+import Components from "views/Components/Components";
+import LandingPage from "views/LandingPage/LandingPage";
+import ProfilePage from "views/ProfilePage/ProfilePage";
+import LoginPage from "views/LoginPage/LoginPage";
+import MapPage from "views/MapPage/MapPage";
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/components" component={Components} />
+      <Route path="/" component={MapPage} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/views/MapPage/MapPage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// core components
+import GoogleMapReact from "google-map-react";
+
+import styles from "assets/jss/custom/views/mapPage.js";
+
+import CustomButton from "components/CustomButtons/Button.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Footer from "components/Footer/Footer";
+
+const useStyles = makeStyles(styles);
+
+export default function MapPage(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.container}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_GOOGLE_API_KEY,
+            language: "en",
+          }}
+          defaultCenter={{
+            lat: 36.395319,
+            lng: 127.403476,
+          }}
+          defaultZoom={5}
+          style={{ width: "100%", height: "100%" }}
+          options={(maps) => {
+            return { mapTypeId: "hybrid" };
+          }}
+        />
+        <div>
+          <div>
+            <CustomInput />
+          </div>
+        </div>
+      </div>
+      {/* <Footer whiteFont /> */}
+      <Footer whiteFont />
+    </div>
+  );
+}
