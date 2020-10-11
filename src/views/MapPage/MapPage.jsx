e30e1ef4d3e58f679fbfead240e3704f5c18162b
bonomoon <moonbonoz@gmail.com>
2020-10-11 13:42:15 +0900

    update: calendar locale

--- src/views/MapPage/MapPage.jsx
+++ src/views/MapPage/MapPage.jsx
@@ -147,6 +147,7 @@ export default function MapPage(props) {
                       }}
                     >
                       <Calendar
+                        locale={"en-US"}
                         value={
                           visibleStartDate
                             ? startDate || new Date()
